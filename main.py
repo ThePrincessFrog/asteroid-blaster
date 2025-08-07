47ef7251c639d07a789deef85e1d8e83a53df8ef
Natasha Collins <theprincessfrog@gmail.com>
2025-08-07 09:25:17 -0600

    adding player data

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import pygame
 from constants import *
-
+from player import Player
 
 def main():
     pygame.init()
     print("Starting Asteroids!")
+    clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
@@ -13,8 +16,9 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
-
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
