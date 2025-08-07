a6f85387785c8447d8fb54361b16c82074549f78
Natasha Collins <theprincessfrog@gmail.com>
2025-08-07 10:55:32 -0600

    Astroids can be shot and split. Still would like to add more later.

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import pygame
+import sys
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
 
 def main():
-    pygame.init()
     print("Starting Asteroids!")
+    pygame.init()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable)
+    Shot.containers = (shots, updatable, drawable)
+
     clock = pygame.time.Clock()
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-    dt = 0
+    asteroidField = AsteroidField()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -16,7 +32,15 @@ def main():
                 return
 
         screen.fill("black")
-        player.draw(screen)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
+        for item in asteroids:
+            if item.collides(player):
+                sys.exit("Game over!")
+            for bullet in shots:
+                if item.collides(bullet):
+                    item.split()
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
